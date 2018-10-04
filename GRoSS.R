f26f1d0f58e78de46f24b484be727fa48e99f7cc
Fernando Racimo <fernandoracimo@gmail.com>
2018-10-04 15:49:15 +0200

    Update GRoSS.R

--- GRoSS.R
+++ GRoSS.R
@@ -6,19 +6,19 @@ source("MultiBranchFunc.R")
 
 # Options
 option_list = list(
-  make_option(c("-e", "--neutfile"), type="character", default=NULL, help="Neutral input file name"),
-  make_option(c("-r", "--graphfile"), type="character", default=NULL, help="Graph R file name"),
-  make_option(c("-d", "--dotfile"), type="character", default=NULL, help="Dot file name"),
-  make_option(c("-o", "--outfile"), type="character", default=NULL, help="Output file")
+  make_option(c("-e", "--inputfile"), type="character", default=NULL, help="Name of input file with SNP allele counts."),
+  make_option(c("-r", "--graphfile"), type="character", default=NULL, help="Graph R file name in qpGraph format; don't use this if using the -d option"),
+  make_option(c("-d", "--dotfile"), type="character", default=NULL, help="Dot file name; don't use this if using the -r option."),
+  make_option(c("-o", "--outfile"), type="character", default=NULL, help="Output file name.")
 ); 
  
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 
 
-if (is.null(opt$neutfile)){
+if (is.null(opt$inputfile)){
   print_help(opt_parser)
-  stop("Neutral file name must be supplied.n", call.=FALSE)
+  stop("Input file name must be supplied.n", call.=FALSE)
 }
 if (is.null(opt$outfile)){
   print_help(opt_parser)
@@ -30,7 +30,7 @@ if (is.null(opt$graphfile) & is.null(opt$dotfile)){
 }
 
 
-neutfile <- opt$neutfile
+neutfile <- opt$inputfile
 graphfile <- opt$graphfile
 dotfile <- opt$dotfile
 outfile <- opt$outfile
